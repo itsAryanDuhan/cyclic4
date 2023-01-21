f6971bfbf7b4c74e3f392a738e5b3233ba121aee
Aryan Duhan <105321528+itsAryanDuhan@users.noreply.github.com>
2023-01-20 23:45:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Aryan Duhan - 148819212");
 });
 
-// setup http server to listen on HTTP_PORT
+// setup http server to liston HTTP_PORT
 app.listen(HTTP_PORT);
